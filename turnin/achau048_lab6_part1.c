31674228233babea5ee92dc51ea4ceeee1924344
Abdullah Chaudhry <56360062+ChauAbdul786@users.noreply.github.com>
2021-10-20 01:58:26 -0700

    Added demo link for part 1

--- turnin/achau048_lab6_part1.c
+++ turnin/achau048_lab6_part1.c
@@ -6,6 +6,8 @@
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
+ *
+ *  Demo Link: https://drive.google.com/file/d/1wN2a3MYHzypRAQGwactMb7o8F4aKEfCQ/view?usp=sharing
  */
 #include <avr/io.h>
 #include "timer.h"
